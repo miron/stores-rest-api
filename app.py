9fc0f7c64cea08e3e202a026258a1326007f29d7
shadowyogi <m.iron@icloud.com>
2022-07-12 10:56:39 +0200

    Fixed not running db.init_app(app) (Was protected by __main__)

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "miron"
-
 api = Api(app)
+db.init_app(app)
 
 @app.before_first_request
 def create_tables():
@@ -53,7 +53,6 @@ api.add_resource(UserRegister, "/register")
 
 
 if __name__ == "__main__":
-    db.init_app(app)
     app.run(port=5000, debug=True)
 
 
