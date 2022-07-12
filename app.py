487a26c0dc69fed6d8251ac813c91d1e80204c67
shadowyogi <m.iron@icloud.com>
2022-07-12 12:53:42 +0200

    Renamed Database URL for newer SQLAlchemy versions

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 # from models.user import UserModel
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "miron"
 api = Api(app)
